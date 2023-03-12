cc5aee64d706ea3151475534de92097d95a23fbd
sbradford <sd_bradford@hotmail.com>
2023-03-12 15:56:02 +0000

    First commit

---
+++ ReadFile.py
@@ -0,0 +1,26 @@
+from striprtf.striprtf import rtf_to_text
+import string
+
+def word_count(str):
+    counts = dict()
+    words = str.split()
+
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    return counts
+
+
+
+with open("input/TestGreekLetter.txt") as infile:
+    text = infile.read()
+    #strip integers
+    text = text.translate({ord(i): None for i in '1234567890'})
+
+print(text)
+print("------- word count ---------")
+print( word_count(text))
+
